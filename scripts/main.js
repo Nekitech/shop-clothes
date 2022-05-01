a3076a17ca97907024cd47db4aaee334a5b6b095
Komnatniy_Koldun <82974466+Volshebnik09@users.noreply.github.com>
2022-05-01 15:09:10 +0300

    template__ideas 100%

--- scripts/main.js
+++ scripts/main.js
@@ -9,11 +9,11 @@ slider({
 
 window.addEventListener('resize',()=>{
 	slider({
-	itemsToView: 4,
-	statusQueryStr: '.others__slider__move-status',
-	bodyQueryStr: '.others__slider__content',
-	leftQueryStr: '.others__slider__move-left',
-	rightQueryStr: '.others__slider__move-right',
-	typeOfStatus:'small'
-})
+		itemsToView: 4,
+		statusQueryStr: '.others__slider__move-status',
+		bodyQueryStr: '.others__slider__content',
+		leftQueryStr: '.others__slider__move-left',
+		rightQueryStr: '.others__slider__move-right',
+		typeOfStatus:'small'
+	})
 })
